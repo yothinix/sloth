49bff6a71984c1dbba5c7216f594576e5d2cae14
Yothin M <m@yothinix.com>
2019-05-25 19:47:27 +0700

    Get file from test result :rocket:

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
         total_time = test_result_dict['testsuite']['@time']
         for testcase in test_result_dict['testsuite']['testcase']:
             class_name = testcase['@classname']
+            file_name= testcase['@file']
             test_name = testcase['@name']
             test_time = testcase['@time']
 
